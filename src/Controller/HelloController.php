24691f6995e8261e0b0631b18b8d4e4a085ff877
太田 圭亮 <keisuke-ota@united.jp>
2019-01-09 17:10:13 +0900

    /helloをphpinfo()見るページにします

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,17 +6,11 @@ use Cake\Datasource\ConnectionManager;
  
 class HelloController extends AppController
 {
- 
+    public $uses = array('User');
+
     public function index()
     {
-        $connection = ConnectionManager::get('default');
-        $results = $connection->execute('SELECT * FROM user')->fetchAll();
-        $existData = false;
-        if(isset($results)){
-            $existData = true;
-        }
-
-        $this->set('results', $results);
-        $this->set('existData',$existData);
+        //$result = $this->User->find('all');
+        $this->set('hoge', 'hoge');
     }
 }
--- src/Template/Hello/index.ctp
+++ src/Template/Hello/index.ctp
@@ -12,21 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:500i|Roboto:300,400,700|Roboto+Mono" rel="stylesheet">
 </head>
 <body class="home">
-<h1>Hello!</h1>
-<p><?php echo $existData; ?></p>
-
-<table>
-    <tr>
-        <th>Id</th>
-        <th>name</th>
-    </tr>
- 
-    <?php foreach ((array)$results as $item): ?>
-    <tr>
-        <td><?= $item->id ?></td>
-        <td><?= $item->name ?></td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+<h1>This is php Info!</h1>
+<?php echo phpinfo();?>
 </body>
 </html>
